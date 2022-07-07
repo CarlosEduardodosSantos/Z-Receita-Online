b18b5c438e0eaea1a03700f88df0711affb83cb2
PedroSposit0 <104908331+PedroSposit0@users.noreply.github.com>
2022-07-07 12:09:04 -0300

    att 07/07 12:08

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   // cnpj: '66677799999999',
-  // apiurl: 'http://apipharma.zclub.com.br/',
+  apiurl: 'http://apipharma.zclub.com.br/',
   // apiurl: 'https://localhost:44371/',
   nome: 'Drogaria Juliana',
   login: 'admin',
   senha: 'zip-super',
 
   cnpj: '33344488888888',
-  apiurl: 'https://z-api666.herokuapp.com/'
+  // apiurl: 'https://z-api666.herokuapp.com/'
 };
